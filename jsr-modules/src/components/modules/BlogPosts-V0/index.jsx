894c87a7c1a5e7c9cf6799ed83a177f36fb3bf54
Brandon Van Dyck <bvandyck@hubspot.com>
2023-11-22 12:19:36 -0500

    Package updates

--- jsr-modules/src/components/modules/BlogPosts-V0/index.jsx
+++ jsr-modules/src/components/modules/BlogPosts-V0/index.jsx
@@ -694,7 +694,7 @@ export const Component = ({
 
 export { fields } from './fields.tsx';
 export const meta = {
-  label: `Blog posts`,
+  label: `PUBLIC: Blog posts`,
   host_template_types: ["BLOG_LISTING"],
   icon: blogIcon,
   categories: ["blog"]
